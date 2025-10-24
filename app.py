3f5cd15ee7ffb7c92714fa12134e89d4b7cd4577
otsugua04 <augusto.gonzalez@fisagrp.com>
2025-10-24 18:36:00 -0500

    Cambiar mensaje Servicio1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/saludo', methods=['GET'])
 def saludo():
     nombre = request.args.get('nombre', 'Mundo')
-    return f'Hola mundo, {nombre}!', 200
+    return f'Hola mundo SERVICIO 1, {nombre}!', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
